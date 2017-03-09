6c30fe38fd719f8278bf61ee239e01c00f7e4c45
Joseph Ian Farillas <joseph.farillas@domandtom.com>
2017-03-09 23:55:50 +0800

    Update files on for tutoruals.

--- exercise-01/src/index.js
+++ exercise-01/src/index.js
@@ -2,13 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Pokedex from './components/Pokedex'
 import { Router, Route, browserHistory } from 'react-router'
+import ApolloClient, { createNetworkInterface } from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
 import 'tachyons'
 import './index.css'
 
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cj025i9j08hir0143kcswu0g7'}),
+})
+
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path='/' component={Pokedex} />
-  </Router>
+  <ApolloProvider client={client}>
+    <Router history={browserHistory}>
+      <Route path='/' component={Pokedex} />
+    </Router>
+  </ApolloProvider>
   ),
   document.getElementById('root')
 )
